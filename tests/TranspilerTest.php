b7b52025d98aa741717c4f27cb8e9a4683ecbc2d
Dario Nuevo <dn@xbe.ch>
2020-01-09 10:36:39 +0100

    add test for forInstance

--- tests/TranspilerTest.php
+++ tests/TranspilerTest.php
@@ -115,6 +115,9 @@ class TranspilerTest extends TestCase {
                 null,
                 [
                 ]
+            ],
+            [
+                "app6forInstance.yml",
             ]
         ];
     }
