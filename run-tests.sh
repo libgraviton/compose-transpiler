a76eebeb6dd360eff2f4d09e436ac49d69648f06
Dario Nuevo <dn@xbe.ch>
2022-01-12 14:10:17 +0100

    update php version

--- run-tests.sh
+++ run-tests.sh
@@ -1,1 +1,1 @@
-docker run --rm -v ${PWD}:/app -w /app -u "$(id -u):$(id -g)" php:8.0-cli php vendor/bin/phpunit tests/
+docker run --rm -v ${PWD}:/app -w /app -u "$(id -u):$(id -g)" php:8.1-cli php vendor/bin/phpunit tests/
